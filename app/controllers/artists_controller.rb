9873afec2d5631310ff0e0a8dc92012d3c74f48b
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-09-04 20:02:18 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,15 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
     @artist = Artist.new
+    
   end
 
   def create
@@ -42,9 +45,13 @@ class ArtistsController < ApplicationController
     redirect_to artists_path
   end
 
+ 
+
   private
 
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  
 end
